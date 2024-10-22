6229672664053b3305c7169dd1564ed56dce734b
matt <22485542+matthewfong3@users.noreply.github.com>
2024-10-22 15:17:30 -0400

    pushing BFS algorithm

---
+++ Graphs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph myGraph = new Graph();
+
+            Console.WriteLine("Depth First Search: ");
+            myGraph.DepthFirst("Living Room");
+            Console.WriteLine();
+            Console.WriteLine("Breadth First Search: ");
+            myGraph.BreadthFirst("Living Room");
+        }
+    }
+}
